febf095443bf1998466c150df6be4ff53ea412cb
Paul Romine <promine@gmail.com>
2023-04-09 19:32:46 -0500

    sketch out pseudocode

---
+++ main.py
@@ -0,0 +1,9 @@
+# for each haste value,
+## load the request json
+## modify its bonus haste
+## write to the temp json
+## run the sim cli
+## get the output
+## parse the json into a python dictionary
+## extract the DPS value
+## store the DPS value in the output dictionary
