dd2af011180a51bbfc383ff89b1d05609d876afa
Paul Romine <promine@gmail.com>
2023-04-11 07:20:43 -0500

    experiment with command piping

--- main.py
+++ main.py
@@ -3,6 +3,12 @@
 ## modify its bonus haste
 ## write to the temp json
 ## run the sim cli
+import os, json
+#stream = os.popen('~/Code/paradigm72-wotlk/wowsimcli-amd64-darwin -input ~/Code/paradigm72-wotlk/eleHaste/P2Ele.json --replace=""')
+stream = os.popen('echo test')
+output = stream.read()
+outputDict = json.loads(output)
+print(outputDict['raidMetrics'])
 ## get the output
 ## parse the json into a python dictionary
 ## extract the DPS value
