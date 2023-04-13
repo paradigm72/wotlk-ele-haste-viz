29ad7200bf43146ebd8cb7ae72559d2dd38ef951
Paul Romine <promine@gmail.com>
2023-04-12 19:29:47 -0500

    piping working

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 # for each haste value,
-## load the request json
-## modify its bonus haste
-## write to the temp json
-## run the sim cli
-import os, json
-#stream = os.popen('~/Code/paradigm72-wotlk/wowsimcli-amd64-darwin -input ~/Code/paradigm72-wotlk/eleHaste/P2Ele.json --replace=""')
-stream = os.popen('echo test')
-output = stream.read()
+# load the request json
+# modify its bonus haste
+# write to the temp json
+# run the sim cli
+import subprocess, json
+process = subprocess.run(['/Users/paradigm72/Code/paradigm72-wotlk/wowsimcli-amd64-darwin',
+                          '-input',
+                          '/Users/paradigm72/Code/paradigm72-wotlk/eleHaste/P2Ele.json',
+                          '--replace='''],
+                         stderr=subprocess.PIPE,
+                         stdout=subprocess.PIPE,
+                         universal_newlines=True)
+# get the output
+output = process.stderr  # why on stderr? :)
+# parse the json into a python dictionary
 outputDict = json.loads(output)
-print(outputDict['raidMetrics'])
-## get the output
-## parse the json into a python dictionary
-## extract the DPS value
-## store the DPS value in the output dictionary
+# extract the DPS value
+dps = outputDict['raidMetrics']['dps']['avg']
+# store the DPS value in the output dictionary
+print(dps)
